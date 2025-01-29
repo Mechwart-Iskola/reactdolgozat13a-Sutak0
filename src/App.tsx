dbae80260b7936deed149bdb4f5de22481031342
Sutak0 <sutak.david.5@gmail.com>
2025-01-29 08:52:24 +0100

    dolgozat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import ProductCard from "./ProductCard";
+import Productcard from './components/Productcard'
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Product Information</h1>
-      <ProductCard />
+      <Productcard />
     </div>
   );
 }
